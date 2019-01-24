89d4ec148e164985ef840cd4857774d7109cbb17
Hadi Ariawan <hd.ariawan@gmail.com>
2019-01-24 23:02:23 +0700

    Provinces, Cities and Areas in Indonesia

---
+++ provinces.php
@@ -0,0 +1,142 @@
+<?php
+
+return [
+    'ID' => [
+        [
+            'name' => 'Aceh',
+            'iso_code' => 'AC',
+        ],
+        [
+            'name' => 'Bali',
+            'iso_code' => 'BA',
+        ],
+        [
+            'name' => 'Banten',
+            'iso_code' => 'BT',
+        ],
+        [
+            'name' => 'Bengkulu',
+            'iso_code' => 'BE',
+        ],
+        [
+            'name' => 'DKI Jakarta',
+            'iso_code' => 'JK',
+        ],
+        [
+            'name' => 'Gorontalo',
+            'iso_code' => 'GO',
+        ],
+        [
+            'name' => 'Jambi',
+            'iso_code' => 'JA',
+        ],
+        [
+            'name' => 'Jawa Barat',
+            'iso_code' => 'JB',
+        ],
+        [
+            'name' => 'Jawa Tengah',
+            'iso_code' => 'JT',
+        ],
+        [
+            'name' => 'Jawa Timur',
+            'iso_code' => 'JI',
+        ],
+        [
+            'name' => 'Kalimantan Barat',
+            'iso_code' => 'KB',
+        ],
+        [
+            'name' => 'Kalimantan Selatan',
+            'iso_code' => 'KS',
+        ],
+        [
+            'name' => 'Kalimantan Tengah',
+            'iso_code' => 'KT',
+        ],
+        [
+            'name' => 'Kalimantan Timur',
+            'iso_code' => 'KI',
+        ],
+        [
+            'name' => 'Kalimantan Utara',
+            'iso_code' => 'KU',
+        ],
+        [
+            'name' => 'Kepulauan Bangka Belitung',
+            'iso_code' => 'BB',
+        ],
+        [
+            'name' => 'Kepulauan Riau',
+            'iso_code' => 'KR',
+        ],
+        [
+            'name' => 'Lampung',
+            'iso_code' => 'LA',
+        ],
+        [
+            'name' => 'Maluku',
+            'iso_code' => 'MA',
+        ],
+        [
+            'name' => 'Maluku Utara',
+            'iso_code' => 'MU',
+        ],
+        [
+            'name' => 'Nusa Tenggara Barat',
+            'iso_code' => 'NB',
+        ],
+        [
+            'name' => 'Nusa Tenggara Timur',
+            'iso_code' => 'NT',
+        ],
+        [
+            'name' => 'Papua',
+            'iso_code' => 'PA',
+        ],
+        [
+            'name' => 'Papua Barat',
+            'iso_code' => 'PB',
+        ],
+        [
+            'name' => 'Riau',
+            'iso_code' => 'RI',
+        ],
+        [
+            'name' => 'Sulawesi Barat',
+            'iso_code' => 'SR',
+        ],
+        [
+            'name' => 'Sulawesi Selatan',
+            'iso_code' => 'SN',
+        ],
+        [
+            'name' => 'Sulawesi Tengah',
+            'iso_code' => 'ST',
+        ],
+        [
+            'name' => 'Sulawesi Tenggara',
+            'iso_code' => 'SG',
+        ],
+        [
+            'name' => 'Sulawesi Utara',
+            'iso_code' => 'SA',
+        ],
+        [
+            'name' => 'Sumatera Barat',
+            'iso_code' => 'SB',
+        ],
+        [
+            'name' => 'Sumatera Selatan',
+            'iso_code' => 'SS',
+        ],
+        [
+            'name' => 'Sumatera Utara',
+            'iso_code' => 'SU',
+        ],
+        [
+            'name' => 'Yogyakarta',
+            'iso_code' => 'YO',
+        ],
+    ]
+];
